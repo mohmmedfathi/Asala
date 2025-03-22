5dea0e498c2da4bda118f8f7c0c6566f8713066a
MohammedFathi <mohmmedfathi.123@gmail.com>
2025-03-23 00:13:42 +0200

    v1.6

--- project/urls.py
+++ project/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
    
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/communities/', include('communities.urls')),
