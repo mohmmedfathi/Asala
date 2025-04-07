388c824d8abfd043d10dbd0f5e37c878643f5d92
MohammedFathi <mohmmedfathi.123@gmail.com>
2025-04-07 23:24:59 +0200

    a

--- products/serializers.py
+++ products/serializers.py
@@ -2,18 +2,11 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
+    # image = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'price', 'image', 'category']
 
-    def get_image(self, obj):
-        request = self.context.get('request')
-        print("DEBUG >>>", request)
-        if obj.image and hasattr(obj.image, 'url'):
-            if request:
-                return request.build_absolute_uri(obj.image.url)
-            return obj.image.url
-        return None
+    
     
